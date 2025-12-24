e7e26ad569b1672068ef381099ac1cede293acbe
kurveayushi <kurveayushi@gmail.com>
2025-12-24 21:48:00 +0530

    connected to mongodb atlas

--- GOAT/backend/server.js
+++ GOAT/backend/server.js
@@ -1,3 +1,5 @@
+import dotenv from "dotenv";
+dotenv.config({ override: true });
 import express from "express";
 import connectDB from "./config/db.js";
 import categoryRoutes from "./routes/categoryRoutes.js";
@@ -6,13 +8,17 @@ import cartRoutes from "./routes/cartRoutes.js";
 // const cors = require("cors");
 import cors from "cors";
 
+// import connectDB from "./db.js";
+
+// Connect DB
+connectDB();
+
 const app = express();
 app.use(express.json());
 app.use(cors());
 app.use("/uploads", express.static("uploads")); // MAKE UPLOADS PUBLIC
 
-// Connect DB
-connectDB();
+
 
 // Serve static category images
 app.use("/images", express.static("public/images"));
